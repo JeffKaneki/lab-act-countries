263ba945485a10c152a1b514c627f3cb37c6d731
iMac01 <imac01@iMac01s-iMac1.local>
2026-09-18 11:11:23 +0800

    Task 2: Added the Dynamic Route(Nested)

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import CountriesPage from "./pages/CountriesPage";
 import BucketListPage from "./pages/BucketListPage";
 import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import CountryDetailPage from "./pages/CountryDetailPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="countries" element={<CountriesPage />} />
+        <Route path="countries">
+          <Route index element={<CountriesPage />} />
+          <Route path=":countryCode" element={<CountryDetailPage />} />
+        </Route>
         <Route path="bucket-list" element={<BucketListPage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="*" element={<NotFoundPage />} />
